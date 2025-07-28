e7fd15b6a4a8f2c1c6811ea0557551d5a35afe4e
Cuong Tran <cuongtv1900tv@gmail.com>
2025-07-29 01:38:28 +0700

    Add startup print and servo test to main.py

---
+++ src/main.py
@@ -0,0 +1,10 @@
+import time
+
+def main():
+    print("Bắt đầu chương trình Hello World")
+    while True:
+        print("Hello World")
+        time.sleep(1)  # Chờ 1 giây
+
+if __name__ == "__main__":
+    main()
