e2cbded7f7ca622d71726e45eaa3c51bc87dea39
Cuong Tran <cuongtv1900tv@gmail.com>
2025-07-29 22:34:25 +0700

    Doi tu 180 thanh 150

--- src/main.py
+++ src/main.py
@@ -5,13 +5,13 @@ kit = ServoKit(channels=8)
 
 while True:
     # Từ 100 đến 180 độ
-    for angle in range(100, 181, 1):
+    for angle in range(150, 181, 1):
         kit.servo[4].angle = angle
         print(f"Servo 4 -> Góc: {angle}°")
         time.sleep(0.02)
 
     # Quay ngược lại từ 180 về 100 độ
-    for angle in range(180, 99, -1):
+    for angle in range(180, 150, -1):
         kit.servo[4].angle = angle
         print(f"Servo 4 <- Góc: {angle}°")
         time.sleep(0.02)
