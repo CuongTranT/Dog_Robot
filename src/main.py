69286d7158cef640eb482b95b9ceeb0a37304265
Cuong Tran <cuongtv1900tv@gmail.com>
2025-07-29 22:43:23 +0700

    calib

--- src/main.py
+++ src/main.py
@@ -4,8 +4,9 @@ from adafruit_servokit import ServoKit
 kit = ServoKit(channels=8)
 
 while True:
-    # Từ 100 đến 180 độ
-    for angle in range(0, 180, 1):
+    # Từ 150 đến 180 độ kenh 4
+    # Từ 0 đến 80 độ kenh 5
+    for angle in range(0, 80, 1):
         # kit.servo[4].angle = angle
         kit.servo[5].angle = angle
         # print(f"Servo 4 -> Góc: {angle}°")
@@ -13,7 +14,7 @@ while True:
         time.sleep(0.02)
 
     # Quay ngược lại từ 180 về 100 độ
-    for angle in range(180, 0, -1):
+    for angle in range(80, 0, -1):
         # kit.servo[4].angle = angle
         kit.servo[5].angle = angle
         # print(f"Servo 4 <- Góc: {angle}°")
