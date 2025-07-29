18e7226ceda78b039b82d5174b7e81175eecc12b
Cuong Tran <cuongtv1900tv@gmail.com>
2025-07-29 23:12:01 +0700

    calib

--- src/main.py
+++ src/main.py
@@ -9,22 +9,23 @@ while True:
         kit.servo[4].angle = angle
         print(f"Servo 4 -> Góc: {angle}°")
         time.sleep(0.02)
-    # Từ 0 đến 80 độ kenh 5
-    for angle in range(0, 80, 1):
-        kit.servo[5].angle = angle
-        print(f"Servo 5 -> Góc: {angle}°")
-        time.sleep(0.02)
 # Quay ngược lại từ 180 về 150 độ
     for angle in range(180, 150, -1):
         kit.servo[4].angle = angle
         print(f"Servo 4 <- Góc: {angle}°")
         time.sleep(0.02)
+    # # Từ 0 đến 80 độ kenh 5
+    # for angle in range(0, 80, 1):
+    #     kit.servo[5].angle = angle
+    #     print(f"Servo 5 -> Góc: {angle}°")
+    #     time.sleep(0.02)
 
-# Quay ngược kênh 5 từ 80 về 0 độ
-    for angle in range(80, -1, -1):
-        kit.servo[5].angle = angle
-        print(f"Servo 5 <- Góc: {angle}°")
-        time.sleep(0.02)
+
+# # Quay ngược kênh 5 từ 80 về 0 độ
+#     for angle in range(80, -1, -1):
+#         kit.servo[5].angle = angle
+#         print(f"Servo 5 <- Góc: {angle}°")
+#         time.sleep(0.02)
 
 
 
