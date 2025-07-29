27df4682846f79f254ddb0d6aef0c20a7197d475
Cuong Tran <cuongtv1900tv@gmail.com>
2025-07-29 22:38:48 +0700

    them lai kenh 5

--- src/main.py
+++ src/main.py
@@ -7,22 +7,17 @@ while True:
     # Từ 100 đến 180 độ
     for angle in range(150, 181, 1):
         kit.servo[4].angle = angle
+        kit.servo[5].angle = angle
         print(f"Servo 4 -> Góc: {angle}°")
+        print(f"Servo 5 -> Góc: {angle}°")
         time.sleep(0.02)
 
     # Quay ngược lại từ 180 về 100 độ
     for angle in range(180, 150, -1):
         kit.servo[4].angle = angle
-        print(f"Servo 4 <- Góc: {angle}°")
-        time.sleep(0.02)
-            # Từ 100 đến 180 độ
-    for angle in range(100, 181, 1):
         kit.servo[5].angle = angle
+        print(f"Servo 4 <- Góc: {angle}°")
         print(f"Servo 5 -> Góc: {angle}°")
         time.sleep(0.02)
 
-    # Quay ngược lại từ 180 về 100 độ
-    for angle in range(180, 99, -1):
-        kit.servo[5].angle = angle
-        print(f"Servo 5 <- Góc: {angle}°")
-        time.sleep(0.02)
+
