68d1ed4212e710e2439c2619f656a027bc47eedd
Cuong Tran <cuongtv1900tv@gmail.com>
2025-07-29 20:32:08 +0700

    new main, delete PCA9685.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
+from PCA9685_smbus2 import PCA9685
 import time
 
-def main():
-    print("Bắt đầu chương trình Hello World")
-    while True:
-        print("Hello World")
-        time.sleep(1)  # Chờ 1 giây
 
-if __name__ == "__main__":
-    main()
+pwm = PCA9685.PCA9685() # use default I2C bus 3, default address 0x40
+pwm.set_pwm_freq(50) # set frequency to 50 Hz for SG90 servos
+
+print("Press Ctrl+C to quit...")
+while True:
+    # set servo to 90 degrees
+    pwm.set_pwm(0, 0, 512) # channel 0, off time 512 (2 ms)
+    time.sleep(1)
