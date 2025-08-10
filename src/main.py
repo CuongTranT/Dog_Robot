fcf391cf271cbd2c72c0645089e19c0358c4a923
Cuong Tran <cuongtv1900tv@gmail.com>
2025-08-11 00:44:30 +0700

    a

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ kit.servo[1].set_pulse_width_range(600, 2400)
 
 # Gán góc:
 kit.servo[0].angle =  70  # độ
-kit.servo[1].angle =  0  # độ
+kit.servo[1].angle =  20  # độ
 print("CH0=45°, CH1=120°")
 #kenh 0: 120 độ
 #kenh 1: 180 độ
