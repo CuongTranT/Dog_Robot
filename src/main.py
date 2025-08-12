3b3c9a5921d8e7e93f103644d317e5d6880686ef
Cuong Tran <cuongtv1900tv@gmail.com>
2025-08-12 23:26:51 +0700

    a

--- src/main.py
+++ src/main.py
@@ -20,10 +20,10 @@ def compute_right_leg(x, y):
     theta = math.atan2(y, x)
     cos_beta = (L1**2 + D**2 - L2**2) / (2 * L1 * D)
     beta = math.acos(cos_beta)
-    alpha_hip = math.degrees(theta - beta)
+    alpha_hip = math.degrees(theta - beta) + 90
     cos_gamma = (L1**2 + L2**2 - D**2) / (2 * L1 * L2)
     gamma = math.acos(cos_gamma)
-    alpha_knee = math.degrees(math.pi - gamma)  # ⚠️ theo yêu cầu
+    alpha_knee = math.degrees(math.pi - gamma) + 90 # ⚠️ theo yêu cầu
     return alpha_knee, alpha_hip
 
 # 🔧 HÀM IK cho chân trái
