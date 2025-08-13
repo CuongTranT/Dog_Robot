cfde5eaaed05a501ebc7a0ba7a01daae19cd8899
Cuong Tran <cuongtv1900tv@gmail.com>
2025-08-13 23:30:59 +0700

    a

--- src/main.py
+++ src/main.py
@@ -17,7 +17,7 @@ def angle2pulse(angle):
 # 🔧 Hàm set kênh 0 quay góc 45 độ
 # ====================
 def set_servo_channel_0_to_45():
-    angle = 180  # độ
+    angle = 170  # độ
     pulse = angle2pulse(angle)
     pwm.set_pwm(0, 0, pulse)
     print(f"✅ Đã set kênh 0 quay tới {angle}° (PWM: {pulse})")
