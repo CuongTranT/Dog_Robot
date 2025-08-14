d4d5583e765d7f28a8f640f2a144bd3e4f59fe0e
Cuong Tran <cuongtv1900tv@gmail.com>
2025-08-14 22:17:11 +0700

    a

--- src/main.py
+++ src/main.py
@@ -155,8 +155,9 @@ def trot_forward(steps=3, dx=60, lift=35, T=0.35):
 
 # ================== DEMO ==================
 if __name__ == "__main__":
-    move_foot_xy('RL', -50, -100, elbow='down')
+    set_leg_angles(leg, 0, 90)
+    # move_foot_xy(leg, 0, -100, elbow='down')
     # stand_all()
     # chỉnh nhanh offset/invert cho đúng cơ khí rồi hãy chạy gait
     # trot_forward(steps=2, dx=50, lift=30, T=0.30)
-    # stand_all()   
+    # stand_all()
