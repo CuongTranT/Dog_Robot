a4b233fd6fc5f8949eca6afa25580a7efa25c18e
Cuong Tran <cuongtv1900tv@gmail.com>
2025-08-15 23:39:08 +0700

    a

--- src/main.py
+++ src/main.py
@@ -172,7 +172,7 @@ if __name__ == "__main__":
     # set_servo_angle(0, 10)  # RF Hip
     # set_servo_angle(1, 10)  # RF Knee
     set_servo_angle(2, 30, 1, 0)  # RR Hip
-    set_servo_angle(3, 50,1,0)  # RR Knee
+    set_servo_angle(3, 70,1,0)  # RR Knee
     # # set_servo_angle(4, 180)  # LF Hip
     # # set_servo_angle(5, 180)  # LF Knee
     # # set_servo_angle(6, 180)  # LR Hip
