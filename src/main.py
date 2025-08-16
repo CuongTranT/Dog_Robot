84edb5127dd138c205f063eb353d5f43747a05ed
Cuong Tran <cuongtv1900tv@gmail.com>
2025-08-16 22:07:55 +0700

    a

--- src/main.py
+++ src/main.py
@@ -148,7 +148,7 @@ string = [(0, -10), (10, -10), (2, -10), (3, -10)]  # Dáng đi
 # ========================
 # ▶️ Vòng lặp điều khiển
 # ========================
-if __name__ == "__main__":
+# if __name__ == "__main__":
     # print("🚀 Đang đưa robot về vị trí khởi động...")  # 🟢 Tự động chuyển về start_pose
     # move_all_legs(start_pose)
     # while True:
